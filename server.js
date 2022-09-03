9ce15061ee1c38bbd5b8c2bc803cfca39dc6f40e
anwarhossainbd <mdanwarhossain1996bangladesh@gmail.com>
2022-09-03 15:43:18 +0600

    server Created

---
+++ server.js
@@ -0,0 +1,32 @@
+
+import { ApolloServer, gql } from 'apollo-server';
+import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core'
+
+const typeDefs = gql`
+   type Query{
+     greet:String
+   }
+
+`
+
+const resolvers ={
+    Query:{
+        greet:()=>  "hello anwar"
+    }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+
+
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground(),
+  ],
+
+})
+
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
